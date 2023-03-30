5c0a559dea79276bd3a5362ff769bfc358c6c61e
Pablo Coz <pablocr_234@hotmail.com>
2023-03-30 17:42:37 -0500

    new-commit

--- src/app/dbz/personaje/personaje.component.ts
+++ src/app/dbz/personaje/personaje.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-personaje',
   templateUrl: './personaje.component.html',
 })
 export class PersonajeComponent {
-
+  @Input() datos: any[] = [];
 }
